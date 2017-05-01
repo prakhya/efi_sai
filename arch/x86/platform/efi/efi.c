4d0a11c4307b011adb6f5ab0b7573b96f1ac7691
Sai Praneeth <sai.praneeth.prakhya@intel.com>
2017-05-01 14:44:38 -0700

    x86/efi: Dont use mm_struct and switch_mm for x86_32
    Since, x86_32 has never used efi_pgd it doesn't make sense to use
    mm_struct and thus swicth_mm() for x86_32.
    
    Signed_off_by: Sai Praneeth Prakhya <sai.praneeth.prakhya@intel.com>

--- arch/x86/platform/efi/efi.c
+++ arch/x86/platform/efi/efi.c
@@ -74,6 +74,7 @@ static int __init setup_add_efi_memmap(char *arg)
 }
 early_param("add_efi_memmap", setup_add_efi_memmap);
 
+#ifdef CONFIG_X86_64
 static efi_status_t __init phys_efi_set_virtual_address_map(
 	unsigned long memory_map_size,
 	unsigned long descriptor_size,
@@ -90,6 +91,33 @@ static efi_status_t __init phys_efi_set_virtual_address_map(
 
 	return status;
 }
+#endif
+
+#ifdef CONFIG_X86_32
+static efi_status_t __init phys_efi_set_virtual_address_map(
+	unsigned long memory_map_size,
+	unsigned long descriptor_size,
+	u32 descriptor_version,
+	efi_memory_desc_t *virtual_map)
+{
+	efi_status_t status;
+	unsigned long flags;
+	union efi_saved_pgd saved_pgd;
+
+	saved_pgd = efi_call_phys_prolog();
+
+	/* Disable interrupts around EFI calls: */
+	local_irq_save(flags);
+	status = efi_call_phys(efi_phys.set_virtual_address_map,
+			       memory_map_size, descriptor_size,
+			       descriptor_version, virtual_map);
+	local_irq_restore(flags);
+
+	efi_call_phys_epilog(saved_pgd);
+
+	return status;
+}
+#endif
 
 void __init efi_find_mirror(void)
 {
